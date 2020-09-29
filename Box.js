70ce732b5a513897bacb8b002dad781378fa03a2
shubham <shubhampatil9992@gmail.com>
2020-09-29 11:45:06 +0530

    Tower_Seige_2

---
+++ Box.js
@@ -0,0 +1,52 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0,
+        isStatic:false
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.visibility=255;
+    
+    World.add(world, this.body);
+  }
+  display(){
+   
+    if(this.body.speed>4){
+
+        World.remove(world,this.body);
+        push();
+        this.visibility=this.visibility-5;
+        tint(255,this.visibility);
+        // rect(pos.x,pos.y,this.width,this.height);
+        pop();
+    }
+    else{
+       var pos =this.body.position;
+       var angle = this.body.angle;
+      if((pos.y>=350 && pos.y<352) || (pos.y>=250 && pos.y<=260)){
+      fill("lightblue");
+      }
+      if((pos.y>=310 && pos.y<=320)||(pos.y>=170 && pos.y<=180)){
+        fill("lightpink");
+      }
+      if((pos.y>=270 && pos.y<=280)||(pos.y>=210 && pos.y<=220)){
+        fill(rgb(78,224,209));
+      }
+      if(pos.y>=230 && pos.y<=240){
+        fill("gray");
+      }
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      //strokeWeight(5);
+    
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  }
+};
--- sketch.js
+++ sketch.js
@@ -1,9 +1,169 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world; 
+function preload(){
+//  polygon_img=loadImage("polygon.png");
+}
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(900,500);
+  engine = Engine.create();
+  world = engine.world;
+
+  // var options={
+  //   isStatic:true
+  // }
+  // Poly=Bodies.circle(200,200,10,options);
+  // World.add(world,Poly);
+
+  Poly=new Pol(100,250,40);
+
+//  console.log(polygon);
+  ground1 = new Ground(310,376,220,10);
+  ground2 = new Ground(600,276,160,10);
+  basement=new Ground(400,height,1000,20);
+
+  // options_pol={
+  //   isStatic:false
+  // }
+  // pol=Bodies.circle(100,200,50,options_pol);
+  // World.add(world,pol);
+
+  
+  slingShot=new Slingshot(Poly.body,{x:100,y:250});
+
+  // level ONE
+  block1=new Box(220,350,30,40);
+  block2=new Box(250,350,30,40);
+  block3=new Box(280,350,30,40);
+  block4=new Box(310,350,30,40);
+  block5=new Box(340,350,30,40);
+  block6=new Box(370,350,30,40);
+  block7=new Box(400,350,30,40);
+
+  // part 2
+  block17=new Box(540,250,30,40);
+  block18=new Box(570,250,30,40);
+  block19=new Box(600,250,30,40);
+  block20=new Box(630,250,30,40);
+  block21=new Box(660,250,30,40);
+
+  // level Two
+  block8=new Box(250,310,30,40);
+  block9=new Box(280,310,30,40);
+  block10=new Box(310,310,30,40);
+  block11=new Box(340,310,30,40);
+  block12=new Box(370,310,30,40);
+
+  // part 2
+  block22=new Box(570,210,30,40);
+  block23=new Box(600,210,30,40);
+  block24=new Box(630,210,30,40);
+
+   // level Three
+   block13=new Box(280,270,30,40);
+   block14=new Box(310,270,30,40);
+   block15=new Box(340,270,30,40);
+
+  // part 2
+   block25=new Box(600,170,30,40);
+
+   // level Four
+   block16=new Box(310,230,30,40);
+//if(gameState==="onlsing"){
+      // textSize(20);
+      // text("Drag the Hexagonal Stone and Release it , to launch it towards the blocks",100,100);
+//  }
+ // else if(gameState==="onfly"){
+  //    textSize(20);
+  //  text("Press space bar to reset",100,200);
+ // }
+  
+ // createSprite(400, 200, 50, 50);
+ Engine.run(engine);
 }
 
 function draw() {
-  background(255,255,255);  
+   Engine.update(engine);
+   background(rgb(55,43,43));
+    textSize(20);
+    fill("white");
+    text("Drag the Hexagonal Stone and Release it , to launch it towards the blocks",100,100);
+    textSize(20);
+    text("Press \"Space\" bar to reset",600,450);
+  // push();
+  // translate(100,250);
+  // rotate(Matter.Body.angle);
+  //  imageMode(CENTER);
+  //  image(polygon_img,0,0,40,40);
+  //  pop();
+  ground1.display(); 
+  ground2.display(); 
+  basement.display();
+
+  
+ // level ONE
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+
+  // part 2
+  block17.display();
+  block18.display();
+  block19.display();
+  block20.display();
+  block21.display();
+
+// level Two
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+
+// part 2
+  block22.display();
+  block23.display();
+  block24.display();
+
+// level Three
+  block13.display();
+  block14.display();
+  block15.display();
+
+// part 2
+   block25.display();
+
+    // level Four
+   block16.display();
+   slingShot.display();
+   Poly.display();
+   
   drawSprites();
+}
+
+function mouseDragged(){
+ // if(gameState=="onsling"){
+      Matter.Body.setPosition(Poly.body,{x:mouseX,y:mouseY});
+ // }
+}
+
+function mouseReleased(){
+//  gameState="onfly";
+  slingShot.fly();
+  // if(gameState!="onfly")
+  //    slingShot.attach(Poly.body);
+  //   gameState="onsling";
+}
+function keyPressed(){
+  if(keyCode===32){
+ //   gameState="onsling";
+    Matter.Body.setPosition(Poly.body,{x:100,y:250});
+    slingShot.attach(Poly.body);
+  }
 }
